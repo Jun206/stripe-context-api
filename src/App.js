a1ba32a543075e9c2df13061da1edc10438515f5
Your Name <you@example.com>
2022-03-10 21:54:49 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar_93'
+import Hero from './Hero_93'
+import Sidebar from './Sidebar_93'
+import Submenu from './Submenu_93'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar/>
+      <Submenu />
+      <Hero />
+      </>
+    
+  )
+}
+
+export default App
